f5fde8e5bb6932c3b44ce34c10d338e7e0501c48
sanyoni <ksyline1997@gmail.com>
2021-03-29 18:19:51 +0900

    feat: createdAt과 modifiedAt이 json으로 변환될 때 이쁘게 포매팅되도록 함

--- src/main/java/com/sanyoni/doongdoong/entity/TimestampedBase.java
+++ src/main/java/com/sanyoni/doongdoong/entity/TimestampedBase.java
@@ -1,5 +1,6 @@
 package com.sanyoni.doongdoong.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -14,8 +15,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimestampedBase {
     @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedAt;
 }
